9c3cc3f0c96211b23a543f1dfa40d7bd0321f38d
gectokot <dmitry.kulak11@gmail.com>
2021-11-14 18:02:42 +0300

    fixed show more button bug

--- src/components/Flights/Flights.tsx
+++ src/components/Flights/Flights.tsx
@@ -1,16 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { v4 as uuid } from "uuid";
 
 import styles from "./Flights.module.scss";
 import Flight from "../Flight/Flight";
-import { FlightResult } from "../../types/types";
+import { FlightResult, FlightsProps } from "../../types/types";
 import Spinner from "../Spinner/Spinner";
 
 
 const Flights = ({
   flights,
-}: { flights: FlightResult[] | null }) => {
-  const [numberOfFlights, setNumberOfFlights] = useState(5);
+  numberOfFlightsToShow,
+  setNumberOfFlightsToShow,
+}: FlightsProps) => {
 
   const renderFlights = (flights: FlightResult[]) => {
     return flights.map(currFlight => {
@@ -22,12 +23,25 @@ const Flights = ({
           price={flight.price.total.amount}
           key={uuid()}
         />);
-    }).slice(0, numberOfFlights);
+    }).slice(0, numberOfFlightsToShow);
   };
 
   const showMore = (e: React.MouseEvent) => {
     e.preventDefault();
-    setNumberOfFlights(numberOfFlights + 5);
+    setNumberOfFlightsToShow(numberOfFlightsToShow + 5);
+  };
+
+  const renderShowMoreButton = (flights: FlightResult[] | null) => {
+    if (flights && flights.length > 0 &&
+      numberOfFlightsToShow < flights.length) {
+      return <button className={styles.showMoreButton} onMouseDown={showMore}>
+        Показать еще
+      </button>;
+    }
+
+    if (flights && flights.length === 0) {
+      return <p className={styles.failure}>Рейсов, увы, нет</p>;
+    }
   };
 
   return (
@@ -35,10 +49,7 @@ const Flights = ({
       {!flights && <Spinner/>}
       {flights && renderFlights(flights)}
 
-      {flights &&
-      <button className={styles.showMoreButton} onMouseDown={showMore}>
-        Показать еще
-      </button>}
+      {renderShowMoreButton(flights)}
     </div>
   );
 };
