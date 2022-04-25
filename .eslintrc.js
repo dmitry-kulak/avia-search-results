bfeec5b5b0cdfe3b4c318c34d2f68c02732deae8
Dmitry Kulak <dmitry.kulak11@gmail.com>
2022-04-26 00:52:29 +0300

    added mobx eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,9 +10,10 @@ module.exports = {
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
+    "plugin:mobx/recommended",
     "prettier",
   ],
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "mobx"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "@typescript-eslint/no-non-null-assertion": "off"
